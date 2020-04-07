7515f1cc7be61de6b8f8087751ea4a182dfaf5f4
kimjiahn <jiahnkimm@gmail.com>
2020-04-07 23:39:45 +0900

    api 불러오기 + ajax

--- react-app2/movie_app/src/App.js
+++ react-app2/movie_app/src/App.js
@@ -4,10 +4,13 @@ import Movie from './Movie';
 
 
 class App extends Component {
+  state = {}
+
   //3초 후 로드
   componentDidMount() {
-    fetch('https://yts.mx/api/v2/list_movies.json?sort_dy=rating')
+    fetch('https://yts.mx/api/v2/list_movies.json?sort_dy=rating');
   }
+  
 //리액트 내장객체와 구분하기 위해 "_" 붙이기
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => ( 
