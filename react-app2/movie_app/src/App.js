8869dd1a7451876cfca0a19faa00d10303de3c4d
kimjiahn <jiahnkimm@gmail.com>
2020-04-08 17:43:13 +0900

    Updating movie

--- react-app2/movie_app/src/App.js
+++ react-app2/movie_app/src/App.js
@@ -13,8 +13,14 @@ class App extends Component {
 
 //리액트 내장객체와 구분하기 위해 "_" 붙이기
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => ( 
-      <Movie title={movie.title} poster={movie.large_cover_image} key={index} />
+    const movies = this.state.movies.map( movie => ( 
+      <Movie 
+      title={movie.title_english} 
+      poster={movie.medium_cover_image} 
+      key={movie.id} 
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+      />
     ))
     return movies;
   }
--- react-app2/movie_app/src/Movie.js
+++ react-app2/movie_app/src/Movie.js
@@ -3,19 +3,37 @@ import propTypes from 'prop-types';
 import './Movie.css';
 
 
-function Movie({title, poster}) {
+function Movie({title, poster, genres, synopsis}) {
     return (
-        <div>
-            <h1>{title}</h1>
-            <MoviePoster poster={poster} />
+        <div className="Movie">
+            <div className="Movie__Colums">
+                <MoviePoster poster={poster} alt={title} />
+            </div>
+            <div className="Movie__Colums">
+                <h2>{title}</h2>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
         </div>
     )
 }
 
-Movie.propTypes= {
-        title: propTypes.string.isRequired,
-        poster: propTypes.string.isRequired
+function MoviePoster({poster, alt}) {
+    return (
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
+    )
 }
+
+function MovieGenre({genre}) {
+    return (
+        <span className="Movie__Genre">{genre} | </span>
+    )
+}
+
 /*
 class MoviePoster extends Component {
 
@@ -31,14 +49,22 @@ class MoviePoster extends Component {
 }
 */
 //축약 (render X, LifeCycles X)
-function MoviePoster({poster}) {
-    return (
-        <img src={poster} alt="Movie Poster" />
-    )
+
+Movie.propTypes= {
+    title: propTypes.string.isRequired,
+    poster: propTypes.string.isRequired,
+    genres: propTypes.array.isRequired,
+    synopsis: propTypes.string.isRequired
 }
 
 MoviePoster.prototype = {
-    poster: propTypes.string.isRequired
+    poster: propTypes.string.isRequired,
+    alt: propTypes.string.isRequired
 }
+
+MovieGenre.propTypes = {
+    genre: propTypes.string.isRequired
+}
+
 
 export default Movie;
