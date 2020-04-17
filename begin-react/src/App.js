ae206c4f3b129ba15e7df66f67b3f5c3894c2d9d
kimjiahn <jiahnkimm@gmail.com>
2020-04-17 22:03:05 +0900

    에러 잡고 수정해야함,,

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,6 +1,10 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useCallback, useMemo } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+
+function countActiveUsers(users) {
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -19,36 +23,56 @@ function App() {
       {
           id: 1,
           username: 'jiahnkim',
-          email: 'kja@kakao.com'
+          email: 'kja@kakao.com',
+          active: true,
       },
       {
           id: 2,
           username: 'velopert',
-          email: 'public.velopert@gmail.com'
+          email: 'public.velopert@gmail.com',
+          active: false,
       },
       {
           id: 3,
           username: 'santJ',
-          email: 'santJ@snow.com'
+          email: 'santJ@snow.com',
+          active: false,
       }
   ]);
 
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
-      ...inputs,
-      // username,
-      // email,
+      // ...inputs
+      username,
+      email,
     };
+    //setUser([...users, user]);
     setUsers(users.concat(user));
     setInputs({
       username: '',
       email: ''
     });
     nextId.current += 1;
-  };
+  }, [users, username, email]);
+
+  const onRemove = useCallback(id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users => users.filter(user => user.id !== id));
+  }, []);
+
+  const onToggle = useCallback(id => {
+    setUsers(users => users.map(
+      user => user.id === id
+      ? { ...user, active: !user.active }
+      : user
+    ));
+  }, []);
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
@@ -58,7 +82,8 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,22 +1,39 @@
 import React from 'react';
 
-function User({ user }) {
+const user = React.memo(function User({ user, onRemove, onToggle }) {
+    const { username, email, id, active } = user;
+
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b style={{
+                color: active ? 'powderblue' : 'black',
+                cursor: 'pointer' 
+            }} onClick={() => onToggle(id)}>{username}</b> &nbsp;
+            <span>({email})</span>
+            <button onClick={() => onRemove(id)}>삭제</button>
+            {/* 
+                삭제버튼을 클릭하면 위 함수를 호출할것이고
+                그 함수는 props로 받아온 onRemove를 id값을
+                prameter로 넣어서 호출을 해준다
+            */}
         </div>
     );
-}
-function UserList({ users }) {
+});
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
             {
                 users.map( user => (
-                    <User user={user} key={user.id} />
+                    <User
+                        user={user}
+                        key={user.id}
+                        onRemove={onRemove}
+                        onToggle={onToggle}
+                    />
                 ))
             }
         </div>
     );
 }
 
-export default UserList;
+export default React.memo(UserList);
